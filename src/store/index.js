c645d8d09c77682ac65ff2be721295bc7e3ababd
conorg000 <conorgould@hotmail.com>
2023-03-17 13:25:42 +0530

    Setup firestore Setup redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducers from "../reducers";
 
-const store = createStore(rootReducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store;
